ec5ea9336ece9bb78b7994464e7a23028ec07265
Cheng Zhang <3ki5tj@gmail.com>
2015-08-24 21:07:49 -0500

    improve mat.h eigvalue routine

--- vct/testeig.c
+++ vct/testeig.c
@@ -1,12 +1,30 @@
 #include "mat.h"
 
-int main(void)
+static void testeig(double a[3][3])
 {
-  double a[3][3] = {{6.67838e6, 4.19371e6, -157685}, {4.19371e6, 2.70432e6, -109931}, {-157685, -109931, 17187.6}};
   double s[3], v[3][3];
   int ret;
+
   ret = meigsys(s, v, a, 1);
-  printf("ret %d\n", ret);
-  printf("eigval: %g, %g, %g\n", s[0], s[1], s[2]);
+  printf("ret %d: eigval: %14.8f, %14.8f, %14.8f, tol %g\n", ret, s[0], s[1], s[2], msolvezero_lasttol);
+  printf("%14.8f %14.8f %14.8f\n", v[0][0], v[0][1], v[0][2]);
+  printf("%14.8f %14.8f %14.8f\n", v[1][0], v[1][1], v[1][2]);
+  printf("%14.8f %14.8f %14.8f\n\n", v[2][0], v[2][1], v[2][2]);
+}
+
+
+
+int main(void)
+{
+  /* for the following matrix, the tolerance must be
+   * increased significantly for a solution */
+  double a1[3][3] = {{ 247146.37794751967886,  46767.34439483784081,-234432.36461535561830},{  46767.34439483784081,   9741.70930413716087, -44517.71041657723981},{-234432.36461535561830, -44517.71041657723981, 223992.19184470266919}};
+
+  /* the two largest eigenvectors of the following matrix are degenerate */
+  double a2[3][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 0.5}};
+
+  testeig(a1);
+  testeig(a2);
+
   return 0;
 }
