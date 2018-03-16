7e2d6e1f64478aaa1edc74e7f3b2fc92d965a1c8
Cheng Zhang <3ki5tj@gmail.com>
2018-03-15 20:18:46 -0500

    second order exponential extrapolation

--- mie/walker.c
+++ mie/walker.c
@@ -34,7 +34,7 @@ typedef struct {
   int q;
   int trjn, trji, *trj;
   long tot, *cnt; /* histogram data */
-  double ent, entc, entc2, ents, ents2;
+  double ent, entc, entcb, ents, entsb;
   double mest;
 } walker_t;
 
@@ -100,7 +100,7 @@ static double ftail(double t, double m)
 static double walker_entropy(walker_t *w, int npart, int npart2, int verbose)
 {
   int trjn = w->trji, ip, blksz, blksz2;
-  double entp = 0, entp2 = 0, entt;
+  double entp = 0, entp2 = 0, entt, entc2;
 
   /* entropy estimate from the entire trajectory */
   entt = walker_ent_seg(w, 0, trjn);
@@ -122,17 +122,37 @@ static double walker_entropy(walker_t *w, int npart, int npart2, int verbose)
    * S = (St*t - Sp*blksz)/(t-blksz); */
   w->ent = entt;
   w->entc = (entt*trjn - entp*blksz)/(trjn - blksz);
-  w->entc2 = (entt*trjn - entp2*blksz2)/(trjn - blksz2);
-  w->ents = (w->entc*blksz - w->entc2*blksz2)/(blksz - blksz2);
+  entc2 = (entt*trjn - entp2*blksz2)/(trjn - blksz2);
+  w->entcb = (w->entc*blksz - entc2*blksz2)/(blksz - blksz2);
 
   /* fitting to log(1+m/2/t) */
   {
     double ds = entt - entp, xp = exp(ds);
-    if ( xp >= 1.99 ) xp = 1.99;
-    w->mest = 2*trjn*(xp - 1)/(1.*trjn/blksz - xp);
-    w->ents2 = entt + log(1+w->mest/2/trjn);
-    //printf("m %g, ds %g=%g-%g, xp %g, S %g -> %g\n", w->mest, ds, entt, entp, xp, entt, w->ents2);
-    //getchar();
+    double ds2 = entt - entp2, xp2 = exp(ds2);
+    double xpc, xpc2, xpcb;
+
+    //double alpha = 0.5, threshold = log((1 + alpha*trjn/blksz)/(1 + alpha));
+    //printf("ds %g vs %g\n", ds, threshold); getchar();
+    //if ( ds < threshold ) {
+    //  w->ents = w->entc;
+    //  w->entsb = w->entcb;
+    //} else
+    {
+      if ( xp >= .99*trjn/blksz ) xp = .99*trjn/blksz;
+        xpc = (trjn - xp*blksz)/(trjn - blksz);
+        w->ents = entt - log(xpc);
+        w->mest = 2*trjn*(xp - 1)/(1.*trjn/blksz - xp);
+
+      if ( xp2 >= .99*trjn/blksz2 ) xp2 = .99*trjn/blksz2;
+      xpc2 = (trjn - xp2*blksz2)/(trjn - blksz2);
+      //ents2 = entt - log(xpc2);
+
+      xpcb = (xpc*blksz - xpc2*blksz2)/(blksz - blksz2);
+      if ( xpcb < 0 ) xpcb = xpc;
+      w->entsb = entt - log(xpcb);
+      //printf("m %g, ds %g,%g xp %g,%g xpc %g,%g S %g -> %g, %g\n", w->mest, ds, ds2, xp, xp2, xpc, xpc2, entt, w->ents, w->entsb);
+      //getchar();
+    }
   }
 
 #if 0
@@ -145,7 +165,7 @@ static double walker_entropy(walker_t *w, int npart, int npart2, int verbose)
     double b = (S2 - S3)*trjn*blksz*blksz2/(blksz - blksz2);
     double a = (S - entt)*trjn + b/trjn;
     printf("2: a %g, S %g,%g, entt %g, %g; entp %g, %g\n", a2, S2, w->entc, entt, S2-a2/trjn, entp, S2-a2/blksz);
-    printf("3: a %g, S %g,%g, entt %g, %g; entp %g, %g\n", a3, S3, w->entc2, entt, S3-a3/trjn, entp2, S3-a3/blksz2);
+    printf("3: a %g, S %g,%g, entt %g, %g; entp %g, %g\n", a3, S3, w->entcb, entt, S3-a3/trjn, entp2, S3-a3/blksz2);
     printf("S: %g/%g, a %g, b %g, entt %g, %g\n", S, w->ents, a, b, entt, S-a/trjn+b/trjn/trjn);
   }
 #endif
@@ -154,10 +174,10 @@ static double walker_entropy(walker_t *w, int npart, int npart2, int verbose)
   {
     double k2 = (entt - entp)/(trjn - blksz);
     double k3 = (entt - entp2)/(trjn - blksz2);
-    double c = (w->entc - w->entc2)/(k3 - k2);
+    double c = (w->entc - w->entcb)/(k3 - k2);
     double S = w->entc + c*k2;
     double a = (S - entt)*(trjn + c);
-    w->ents2 = S;
+    w->entsb = S;
     w->mest = 2 * (a - c);
     //printf("S %g, a %g, c %g, entt %g, %g; entp %g, %g\n", S, a, c, entt, S-a/trjn/(1+c/trjn), entp, S-a/blksz/(1+c/blksz));
   }
@@ -185,7 +205,7 @@ static double walker_entropy(walker_t *w, int npart, int npart2, int verbose)
       //w->mest *= delr/del*del2r/del2;
       //if ( fabs(del/delr*del2/del2r - 1) < 0.01 ) break;
     }
-    w->ents2 = entt + tailt;
+    w->entsb = entt + tailt;
     if ( verbose ) {
       getchar();
     }
@@ -205,9 +225,9 @@ static void walker_close(walker_t *w)
 static void work(void)
 {
   walker_t **w;
-  av_t avent[1], aventc[1], aventc2[1], avents[1], avents2[1], avmest[1];
-  double ent, entc, entc2, ents, ents2, mest;
-  double var, varc, varc2, vars, vars2, mvar;
+  av_t avent[1], aventc[1], aventcb[1], avents[1], aventsb[1], avmest[1];
+  double ent, entc, entcb, ents, entsb, mest;
+  double var, varc, varcb, vars, varsb, mvar;
   int i;
   long t;
   FILE *fplog;
@@ -231,30 +251,31 @@ static void work(void)
     if ( t % nstrep == 0 ) {
       av_clear(avent);
       av_clear(aventc);
-      av_clear(aventc2);
+      av_clear(aventcb);
       av_clear(avents);
-      av_clear(avents2);
+      av_clear(aventsb);
       av_clear(avmest);
       for ( i = 0; i < nsys; i++ ) {
         walker_entropy(w[i], npart, npart2, i <= 0);
         av_add(avent,  w[i]->ent);
         av_add(aventc, w[i]->entc);
-        av_add(aventc2, w[i]->entc2);
+        av_add(aventcb, w[i]->entcb);
         av_add(avents, w[i]->ents);
-        av_add(avents2, w[i]->ents2);
+        av_add(aventsb, w[i]->entsb);
         av_add(avmest, w[i]->mest);
       }
       ent  = av_getave(avent,  &var);
       entc = av_getave(aventc, &varc);
-      entc2 = av_getave(aventc2, &varc2);
+      entcb = av_getave(aventcb, &varcb);
       ents = av_getave(avents, &vars);
-      ents2 = av_getave(avents2, &vars2);
+      entsb = av_getave(aventsb, &varsb);
       mest = av_getave(avmest, &mvar);
       printf("%9ld: entropy %8.4f(%g), %8.4f, %8.4f; %8.4f, %8.4f(%8.4f), m %6.0f(%5.0f)\n",
-          t, ent, var, entc, entc2, ents, ents2, log(q), mest, sqrt(mvar));
+          t, ent, var, entc, entcb, ents, entsb, log(q), mest, sqrt(mvar));
       fprintf(fplog, "%ld\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\t%g\n", t,
-          ent, sqrt(var), entc, sqrt(varc), log(q),
-          entc2, sqrt(varc2), ents, sqrt(vars), ents2, sqrt(vars2),
+          ent, sqrt(var), log(q),
+          entc, sqrt(varc), entcb, sqrt(varcb),
+          ents, sqrt(vars), entsb, sqrt(varsb),
           mest, sqrt(mest));
       fflush(fplog);
     }
